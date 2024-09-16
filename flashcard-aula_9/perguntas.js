d1be6430fc91c0216f77b9b7e92d4448d2c86ff9
ohashiyukari <137419046+ohashiyukari@users.noreply.github.com>
2024-09-16 08:42:59 -0300

    Add files via upload

---
+++ flashcard-aula_9/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é um algoritmo?',
+    'Um algoritmo é uma sequencia de instruções simples que tem como objetivo obter um resultado. É basicamente uma receita.'
+)
+
+criaCartao(
+    'Geografia',
+    'Quais são as consequências do desmatamento?',
+    'desequilíbrio ambiental provocado pela perda da vegetação nativa. A remoção da vegetação provoca uma grande perda da biodiversidade assim como a perda do habitat de animais e plantas, e, ainda, impacta diretamente na elevação do número de espécies em extinção.'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual a diferença entre células eucarontes e procariontes?',
+    ' As procarióticas caracterizam-se por terem uma estrutura simples e a ausência de núcleo, enquanto as eucarióticas têm um núcleo definido e uma estrutura complexa.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
